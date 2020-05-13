1fe50a24acc0689a2e48c1654bc7d1ee1bee7510
luisfernandocalvillo <62779857+luisfernandocalvillo@users.noreply.github.com>
2020-05-13 02:45:10 -0600

    Add files via upload

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -4,5 +4,31 @@ using namespace std;
 
 int main()
 {
+    string melon;
+    string producto;
+    int cantidad, b;
+    double precio_melon, total;
+
+    producto = melon; 
+
+    melon = b;
+
+    b = 4;
+
+    precio_melon = b;
+    
+    cout << "bienvenido hay venta de melones" << endl << endl;
+    cout << "estos son los precios:" << endl << endl;
+    cout << "melon: " <<precio_melon << endl << endl;
+    cout << "que desea comprar: ";
+    // escribir melon ya que es la unica opcion, se suponia iban a ver mas frutas
+    cin >> producto;
+    cout << "que cantidad desea llevar: ";
+    cin >> cantidad;
+    cout << "este es su total: " << endl << endl;
+    cout << "producto: " << producto << endl;
+    cout << "cantidad: " << cantidad << endl;
+    total = (cantidad*precio_melon);
+    cout << "total a pagar: " << total;
     
 }
